9dc92eaaf661c56477b77564d6163a4ab3b96ec9
Tansu Dasli <tansudasli@gmail.com>
2019-05-16 13:26:04 +0300

    algorithm prepared for sales transaction analysis

---
+++ sales-transaction-ops.py
@@ -0,0 +1,21 @@
+import logging
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+
+# logging
+root = logging.getLogger()
+root.setLevel(logging.INFO)
+
+# create a pipeline, read data from csv
+
+# extract product-type and price
+
+# extract customer-type, then print
+
+# group prices by product-type, then print
+
+# find average of prices by product-group, then print
+
+# find transaction by customer-type, then print
+
+# write output to a file
