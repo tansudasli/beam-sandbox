3cde757e61dd35ee60d0e0de5c8127124ef2406f
Tansu Dasli <tansudasli@gmail.com>
2019-05-16 15:08:26 +0300

    GCP params made dynamic

--- line-count-dataflow.sh
+++ line-count-dataflow.sh
@@ -3,9 +3,10 @@
 # script to run line-count on GCP dataflow
 
 export PROJECT_ID=sandbox-236618
+export FILE_NAME=line-count
 
 python line-count.py \
         --project ${PROJECT_ID} \
         --runner DataFlowRunner \
-        --staging_location gs://beam-pipelines-123/line-count/staging \
-        --temp_location gs://beam-pipelines-123/line-count/temp
+        --staging_location gs://beam-pipelines-123/${FILE_NAME}/staging \
+        --temp_location gs://beam-pipelines-123/${line-count}/temp
--- line-count.py
+++ line-count.py
@@ -11,14 +11,26 @@ root.setLevel(logging.INFO)
 p = beam.Pipeline(options=PipelineOptions())
 
 # source data
-lines = (p | "create sample data" >> beam.Create(["Another episode of Star Wars",
-                                                  "A long time ago in a galaxy far far away"]))
+lines = (
+            p
+            | "create sample data"
+              >> beam.Create(["Another episode of Star Wars",
+                              "A long time ago in a galaxy far far away"])
+        )
 
 #
-line_count = (lines | "count lines" >> beam.CombineGlobally(beam.combiners.CountCombineFn()))
+line_count = (
+                  lines
+                  | "count lines"
+                    >> beam.CombineGlobally(beam.combiners.CountCombineFn())
+             )
 
 #
-(line_count | "printing to log" >> beam.ParDo(lambda (c): logging.info("\n***\ntotal lines: %s \n***", c)))
+(
+        line_count
+        | "printing to log"
+          >> beam.ParDo(lambda (c): logging.info("\n***\ntotal lines: %s \n***", c))
+)
 
 result = p.run()
 
