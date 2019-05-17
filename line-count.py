499ff8d109bd1c020864369d36dce3f60f088b77
Tansu Dasli <tansudasli@gmail.com>
2019-05-17 22:38:06 +0300

    minor update

--- line-count.py
+++ line-count.py
@@ -11,27 +11,25 @@ root.setLevel(logging.INFO)
 p = beam.Pipeline(options=PipelineOptions())
 
 # source data
-lines = (
-            p
-            | "create sample data"
-              >> beam.Create(["Another episode of Star Wars",
-                              "A long time ago in a galaxy far far away"])
-        )
+lines = (p
+         | "create sample data"
+         >> beam.Create(["Another episode of Star Wars",
+                         "A long time ago in a galaxy far far away"])
+         )
 
 #
-line_count = (
-                  lines
-                  | "count lines"
-                    >> beam.CombineGlobally(beam.combiners.CountCombineFn())
-             )
+line_count = (lines
+              | "count lines"
+              >> beam.CombineGlobally(beam.combiners.CountCombineFn())
+              )
 
 #
-(
-        line_count
-        | "printing to log"
-          >> beam.ParDo(lambda (c): logging.info("\n***\ntotal lines: %s \n***", c))
-)
+(line_count
+ | "printing to log"
+ >> beam.ParDo(lambda (c): logging.info("\n***\ntotal lines: %s \n***", c))
+ )
 
+# run
 result = p.run()
 
 result.wait_until_finish()
