c5d423f15742f91bd9fdf1567804b388909cb40b
Tansu Dasli <tansudasli@gmail.com>
2019-05-17 22:39:20 +0300

    minor update

--- average-prices-by-product.sh
+++ average-prices-by-product.sh
@@ -3,10 +3,11 @@
 # script to run line-count on GCP dataflow
 
 export PROJECT_ID=sandbox-236618
-export FILE_NAME=sales-transaction-ops
+export FILE_NAME=average-prices-by-product
 
-python sales-transaction-ops.py \
+python ${FILE_NAME}.py \
         --project ${PROJECT_ID} \
+        --job_name ${FILE_NAME} \
         --runner DataFlowRunner \
         --staging_location gs://beam-pipelines-123/${FILE_NAME}/staging \
         --temp_location gs://beam-pipelines-123/${FILE_NAME}/temp
