5088d98ba13e1b35636eabd16aec14edfc209410
Tansu Dasli <tansudasli@gmail.com>
2019-05-18 13:52:26 +0300

    same code w/ average-prices-by-product in a more clean way.

---
+++ average-prices-by-product-enhanced.py
@@ -0,0 +1,42 @@
+
+import logging
+import apache_beam as beam
+
+from apache_beam.options.pipeline_options import PipelineOptions
+
+
+# logging
+root = logging.getLogger()
+root.setLevel(logging.INFO)
+
+
+# create a pipeline, read data from csv
+pipeline_options = beam.Pipeline(options=PipelineOptions())
+
+
+# extract product-type and price
+class ExtractProductTypePrice(beam.DoFn):
+
+    def process(self, element, *args, **kwargs):
+        line = element.split(',')
+        return [(line[2], float(line[3]))]
+
+
+(pipeline_options
+ | "Read Transaction CSV"
+ >> beam.io.ReadFromText("gs://spark-dataset-1/datasets/sales/sales_transactions.csv")
+ | "Extracting product type and prices"
+ >> beam.ParDo(ExtractProductTypePrice())
+ | "Grouping by product type"
+ >> beam.GroupByKey()
+ | "Product Prices by Product Type"
+ >> beam.ParDo(lambda (k, v): logging.info("Product Type %s = Prices %s", k, str(v)))
+ )
+
+
+# run
+pipeline_options.run().wait_until_finish()
+
+
+
+
