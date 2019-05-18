e85d89024e010a38310d7c331610d6437c55e7e4
Tansu Dasli <tansudasli@gmail.com>
2019-05-18 23:03:43 +0300

    word-count analysis w/ bigger file

---
+++ word-count-enhanced.py
@@ -0,0 +1,40 @@
+import logging
+import apache_beam as beam
+
+from apache_beam.options.pipeline_options import PipelineOptions
+# set logging level
+root = logging.getLogger()
+root.setLevel(logging.INFO)
+
+# create a pipeline
+p = beam.Pipeline(options=PipelineOptions())
+
+
+class ExtractWordCount(beam.DoFn):
+
+    def process(self, element, *args, **kwargs):
+        yield (element.lower(), 1)
+
+
+lines = (p
+         | "Read Text File"
+         >> beam.io.ReadFromText("datasets/words/book.txt")
+         | "Get Words"
+         >> beam.FlatMap(lambda line: line.split())
+         | "Map"
+         # >> beam.Map(lambda word: (word, 1))  #both Map and ParDo is ok
+         >> beam.ParDo(ExtractWordCount())
+         | "Count words"
+         >> beam.GroupByKey()
+         | "Sum of words"
+         >> beam.Map(lambda (k, v): (k, sum(v)))
+         | "Write output File"
+         >> beam.io.WriteToText("datasets/words/book_output.txt")
+         )
+
+(lines
+ | "Print Word Counts"
+ >> beam.ParDo(lambda (c): logging.info("Lines %s ", c))
+ )
+
+p.run().wait_until_finish()
