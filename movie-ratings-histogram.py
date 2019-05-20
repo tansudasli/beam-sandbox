bceeca0b4134eda67ed9c3c5a3b778ffcfa160ee
Tansu Dasli <tansudasli@gmail.com>
2019-05-20 12:14:00 +0300

    movie ratings histogram

---
+++ movie-ratings-histogram.py
@@ -0,0 +1,41 @@
+import logging
+import apache_beam as beam
+
+from apache_beam.options.pipeline_options import PipelineOptions
+
+# set logging level
+root = logging.getLogger()
+root.setLevel(logging.INFO)
+
+# create a pipeline
+p = beam.Pipeline(options=PipelineOptions())
+
+# user-id,movie-id,rating,timestamp
+lines = (p
+         | "Read Text File : Weather"
+         >> beam.io.ReadFromText("datasets/ml-100k/u.data")
+         | "Split Data"
+         >> beam.Map(lambda line: line.split("\t"))
+         | "Get Ratings"
+         >> beam.Map(lambda line: (line[2], 1))
+         | "Count by Value"
+         >> beam.GroupByKey()
+         | "Count"
+         >> beam.Map(lambda (k, v): (k, len(v)))
+         )
+
+
+# the aim is: rating, count-by-value
+(lines
+ | "Print Weather Data"
+ >> beam.ParDo(lambda (c): logging.info("Lines %s ", c))
+ )
+
+# (lines
+#  | "Print Line Count"
+#  >> beam.CombineGlobally(beam.combiners.CountCombineFn())
+#  | "Print line count for %s"
+#  >> beam.ParDo(lambda (c): logging.info("\nTotal line count = %s \n", c))
+#  )
+
+p.run().wait_until_finish()
