5c11744c009f7adc18786e7cec51cc835668a1ad
Tansu Dasli <tansudasli@gmail.com>
2019-05-20 22:15:42 +0300

    split() replaced w/ regex, and flatmap() replaced w/ PTransform class

--- word-count-enhanced.py
+++ word-count-enhanced.py
@@ -1,11 +1,11 @@
 import logging
 import apache_beam as beam
+import re
 
 from apache_beam.options.pipeline_options import PipelineOptions
 
 # set logging level
-root = logging.getLogger()
-root.setLevel(logging.INFO)
+logging.getLogger().setLevel(logging.INFO)
 
 # create a pipeline
 p = beam.Pipeline(options=PipelineOptions())
@@ -16,12 +16,19 @@ class ExtractWordCount(beam.DoFn):
     def process(self, element, *args, **kwargs):
         yield (element.lower(), 1)
 
+class SplitWords(beam.PTransform):
+
+    def expand(self, pcoll):
+        return (pcoll
+                | beam.FlatMap(lambda line: re.findall(r'[A-Za-z]+', line))
+                )
+
 
 lines = (p
          | "Read Text File"
          >> beam.io.ReadFromText("datasets/words/book.txt")
          | "Get Words"
-         >> beam.FlatMap(lambda line: line.split())
+         >> SplitWords()
          | "Map"
          # >> beam.Map(lambda word: (word, 1))  #both Map and ParDo is ok
          >> beam.ParDo(ExtractWordCount())
